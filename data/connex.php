996336102d42f34bf22cf4edd93723377775d838
Haroun El Alami <haroun.dev@gmail.com>
2015-12-29 18:50:02 +0100

    Infos sur la base de données

--- data/connex.php
+++ data/connex.php
@@ -2,10 +2,10 @@
 /**
  * Sert a etablir une connexion a la base de données
  */
-$hostname = "localhost";
-$dbname = "YHEventApp";
-$username = "root";
-$password = "";
+$hostname = "uf63wl4z2daq9dbb.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+$dbname = "jjsoiprm30psalxm";
+$username = "njylx72mppl2kgcu";
+$password = "k8yc8wiqxemq4gni";
 try {
     $db = new PDO("mysql:host=$hostname;dbname=$dbname;charset=utf8", $username, $password);
     //echo("db connecté !!<br>");
